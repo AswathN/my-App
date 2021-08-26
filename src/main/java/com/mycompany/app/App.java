30afbcc42e3c377acbaccc28f9e63ce5dbc0d024
qaautomated <qaautomated@AEA-QA09.aeaonline.net>
2021-08-26 09:54:14 -0400

    Latest Changes for Sauce Integration

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,14 @@
 package com.mycompany.app;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -13,14 +17,26 @@ import io.github.bonigarcia.wdm.WebDriverManager;
  *
  */
 public class App {
-	private static WebDriver driver;
+	private static RemoteWebDriver driver;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		try {
 			System.out.println("<<<<Test Starts>>>>");
 			WebDriverManager.chromedriver().setup();
 			//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
-			driver = new ChromeDriver();
+			//driver = new ChromeDriver();
+			String Sauce_Username = System.getenv("SAUCE_USERNAME");
+			//String Sauce_Password = System.getenv("Sauce_Password");
+			String Sauce_Accesskey = System.getenv("SAUCE_ACCESS_KEY");
+			//String Sauce_Tunnel = "Purposefinancial_tunnel"/*System.getenv("Tunnel")*/;
+			String Sauce_url = "https://" + Sauce_Username + ":" + Sauce_Accesskey;
+			MutableCapabilities capabilities = new MutableCapabilities();
+			capabilities.setCapability("goog:chromeOptions", new ChromeOptions());
+			capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+			capabilities.setCapability("platformName", "Windows 10");
+			capabilities.setCapability("browserVersion", "latest");
+			capabilities.setCapability("browserName", "Chrome");
+			driver = new RemoteWebDriver(new URL(Sauce_url), capabilities);
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			System.out.println("Launching Web Page");
 			driver.get("https://www.google.com/");
